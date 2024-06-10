fe2cd662339d18fcc1ecf4f4f3fb07c9c6e989aa
Advit <advitsingh.4862@gmail.com>
2024-06-10 18:40:31 +0530

    ok

--- client.js
+++
@@ -1,8 +0,0 @@
-fetch('/api/ip')
-  .then(response => response.json())
-  .then(data => {
-    document.getElementById('ip-address').textContent = data.ip;
-  })
-  .catch(error => {
-    console.error('Error fetching IP address:', error);
-  });
